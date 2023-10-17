d43cb165afe4f174282ae867824970570c3c5185
7074220 <7074220@naver.com>
2023-10-17 20:20:58 +0900

    //////

--- src/main/java/com/itwill/entity/Coupon.java
+++ src/main/java/com/itwill/entity/Coupon.java
@@ -1,5 +1,30 @@
 package com.itwill.entity;
 
-public class Coupon {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Coupon {
+     @Id
+     @GeneratedValue(strategy = GenerationType.SEQUENCE )
+	 private Long couponId;
+     
+	 private String couponName;
+	 private String couponDiscount;
+	 private String couponExpirationDate;
+	 private String couponPayday;
+	 @OneToOne
+	 private Userinfo userinfo = new Userinfo();
 }
--- src/main/java/com/itwill/entity/MyPet.java
+++ src/main/java/com/itwill/entity/MyPet.java
@@ -6,6 +6,8 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
@@ -19,6 +21,7 @@ import lombok.NoArgsConstructor;
 public class MyPet {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long mypetNo;
 	@Column(nullable = false)
 	private String mypetName;
