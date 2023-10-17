09fcbd49dd9f1eb345b6b7963b1a7d6bc5236e87
윤호진 <ghwls1997@naver.com>
2023-10-17 18:53:34 +0900

    sss

--- src/main/java/com/itwill/dao/CartDao.java
+++ src/main/java/com/itwill/dao/CartDao.java
@@ -2,4 +2,6 @@ package com.itwill.dao;
 
 public class CartDao {
 
+	
+	
 }
--- src/main/java/com/itwill/entity/Cart.java
+++ src/main/java/com/itwill/entity/Cart.java
@@ -1,5 +1,35 @@
 package com.itwill.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.builder.ToStringExclude;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cart {
 
+	private Long cartNo;
+	private Integer cartQty;
+	
+	@OneToOne
+	@ToStringExclude
+	private Userinfo userinfo = new Userinfo();
+	
+	
+	@OneToMany(mappedBy = "cart")
+	@ToStringExclude
+	
+	private List<Product> products = new ArrayList<>();
 }
