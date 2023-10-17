0518671f2c44e3e56f6b6ab3fa9a2f5e0ba30404
ITWILL <choiyongyong0813@gmail.com>
2023-10-17 18:55:28 +0900

    visit

--- src/main/java/com/itwill/entity/Visit.java
+++ src/main/java/com/itwill/entity/Visit.java
@@ -1,5 +1,45 @@
 package com.itwill.entity;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Visit {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long visitNo;//PK
+
+	private Long visitTime;
+
+	private LocalDate visitDate;
+
+	private String visitCenter;
+	
+	
+	@Builder.Default
+	@ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_Id")
+	@ToString.Exclude
+	private  Userinfo userinfo = new Userinfo();
+			
 }
