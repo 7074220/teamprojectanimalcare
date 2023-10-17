c23571363966a387c8a0123ea29e15810f806640
harucanana <harucanana@gmail.com>
2023-10-17 20:05:28 +0900

    엔터티

--- src/main/java/com/itwill/entity/Volunteer.java
+++ src/main/java/com/itwill/entity/Volunteer.java
@@ -1,5 +1,43 @@
 package com.itwill.entity;
 
-public class Volunteer {
+import java.time.LocalDate;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Volunteer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long volunteerNo; // PK
+	
+	private Long volunteerTime;
+	
+	private LocalDate volunteerDate;
+	
+	private String volunteerCenter;
+	
+	
+	@Builder.Default
+	@ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_id")
+	@ToString.Exclude
+	private Volunteer volunteer = new Volunteer();
+	
+	
 }
