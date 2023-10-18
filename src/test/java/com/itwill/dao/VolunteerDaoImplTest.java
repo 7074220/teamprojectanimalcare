36cd43f3a095f2539f93f20d53e72e6b19021e42
harucanana <harucanana@gmail.com>
2023-10-18 20:15:30 +0900

    ..

---
+++ src/test/java/com/itwill/dao/VolunteerDaoImplTest.java
@@ -0,0 +1,40 @@
+package com.itwill.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class VolunteerDaoImplTest {
+
+	@Test
+	@Disabled
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@Disabled
+	void testInsertVolunteer() {
+		
+	}
+
+	@Test
+	@Disabled
+	void testSelectVolunteer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@Disabled
+	void testUpdateVolunteer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	@Disabled
+	void testDeleteVolunteer() {
+		fail("Not yet implemented");
+	}
+
+}
