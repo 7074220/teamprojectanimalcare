3a966e209b34a30ae3a5bd5e588a975c8d652b37
ahiivii <axoxoh48@naver.com>
2023-10-19 15:28:01 +0900

    cart 몰라

---
+++ src/test/java/com/itwill/dao/CartDaoImplTest.java
@@ -0,0 +1,24 @@
+package com.itwill.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import com.itwill.TeamprojectAnimalcareApplicationTest;
+import com.itwill.entity.Cart;
+import com.itwill.entity.Product;
+import com.itwill.entity.Userinfo;
+
+class CartDaoImplTest extends TeamprojectAnimalcareApplicationTest {
+
+	
+	@Test
+	@Disabled
+	void insertCartTest() {
+		Cart cart = Cart.builder()
+				
+				.build();
+	}
+	
+}
