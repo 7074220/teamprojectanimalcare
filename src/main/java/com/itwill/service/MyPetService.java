974c5c9091ddebeb9e91f87a47be63fc1e0503ad
heoseungbum <heoseungbum@naver.com>
2023-10-19 17:18:16 +0900

    Merge branch 'master' of https://github.com/2023-05-JAVA-DEVELOPER-143/2023-05-JAVA-DEVELOPER-final-project-team2.git

--- src/main/java/com/itwill/service/MyPetService.java
+++ src/main/java/com/itwill/service/MyPetService.java
@@ -1,16 +1,21 @@
 package com.itwill.service;
 
+import java.util.List;
+
 import com.itwill.entity.MyPet;
 
 public interface MyPetService {
 	
-	// 정보등록
+	// 마이펫 등록
 	public MyPet Create(MyPet myPet);
 	
-	// 정보 업데이트
+	// 마이펫 업데이트
 	public MyPet Update(MyPet myPet);
 	
-	// 정보 삭제
+	// 마이펫 삭제
+	public void Delete(Long myPetNo);
 	
+	// 마이펫 리스트
+	public List<MyPet> findAll();
 	
 }
--- src/main/java/com/itwill/service/MyPetServiceImpl.java
+++ src/main/java/com/itwill/service/MyPetServiceImpl.java
@@ -1,15 +1,42 @@
 package com.itwill.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.itwill.dao.MyPetDao;
+import com.itwill.entity.MyPet;
 
 @Service
 public class MyPetServiceImpl implements MyPetService{
 	
 	@Autowired
 	MyPetDao myPetDao;
+	
+	// 마이펫 입력
+	@Override
+	public MyPet Create(MyPet myPet) {
+		return myPetDao.CreatePet(myPet);
+	}
+	
+	// 마이펫 삭제
+	@Override
+	public void Delete(Long myPetNo) {
+		myPetDao.DeletePet(myPetNo);
+	}
+	
+	// 마이펫 수정
+	@Override
+	public MyPet Update(MyPet myPet) {
+		return myPetDao.UpdatePet(myPet);
+	}
+	
+	// 마이펫 리스트
+	@Override
+	public List<MyPet> findAll() {
+		return myPetDao.findAll();
+	}
 	
 	
 	
