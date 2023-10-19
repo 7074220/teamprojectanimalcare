8c79660c5f0b54f23e33fdf8437e04d03a237eaa
ahiivii <axoxoh48@naver.com>
2023-10-19 11:01:12 +0900

    product list test 완료

--- src/test/java/com/itwill/dao/ProductDaoImplTest.java
+++ src/test/java/com/itwill/dao/ProductDaoImplTest.java
@@ -24,11 +24,12 @@ class ProductDaoImplTest extends TeamprojectAnimalcareApplicationTest {
 	@Disabled
 	void insertProductTest() {
 		Product product1 = Product.builder()
-				.productName("츄르")
+				.productName("츄르_멸치맛")
 				.productPrice(3000)
 				.productCategory("간식")
-				.productAmount(3)
+				.productAmount(2)
 				.productImage("cat.jpg")
+				.productStarAvg(2)
 				.build();
 		Product savedProduct1 = productDao.insertProduct(product1);
 		System.out.println(savedProduct1);
@@ -37,14 +38,40 @@ class ProductDaoImplTest extends TeamprojectAnimalcareApplicationTest {
 	
 	@Test
 	@Disabled
+	// 일부 단어 입력으로 제품 검색
 	void findByContainsTest() {
 		List<Product> findProduct = productDao.findByContains("츄");
 	}
 	
 	@Test
-	//@Disabled
+	@Disabled
+	// 높은 가격순 정렬
 	void findAllByOrderByProductPriceDesc() {
 		List<Product> products = productDao.findAllByOrderByProductPriceDesc();
+		System.out.println(products);
+	}
+	
+	@Test
+	@Disabled
+	// 낮은 가격순 정렬
+	void findAllByOrderByProductPriceAsc() {
+		List<Product> products = productDao.findAllByOrderByProductPriceAsc();
+		System.out.println(products);
+	}
+	
+	@Test
+	@Disabled
+	// 평점높은순 정렬
+	void findAllByOrderByProductStarAvgDesc() {
+		List<Product> products = productDao.findAllByOrderByProductStarAvgDesc();
+		System.out.println(products);
+	}
+	
+	@Test
+	@Disabled
+	// 최신번호순 정렬
+	void findAllByOrderByProductNoDesc() {
+		List<Product> products = productDao.findAllByOrderByProductNoDesc();
 		System.out.println(products);
 	}
 }
