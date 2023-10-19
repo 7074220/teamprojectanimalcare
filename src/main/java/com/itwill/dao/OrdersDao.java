92936a35420099b92d82cd460975d75f1f236b95
ahiivii <axoxoh48@naver.com>
2023-10-19 12:00:56 +0900

    orders 이름 변경

--- src/main/java/com/itwill/dao/OrdersDao.java
+++ src/main/java/com/itwill/dao/OrdersDao.java
@@ -1,5 +1,5 @@
 package com.itwill.dao;
 
-public interface OrderDao {
+public interface OrdersDao {
 
 }
--- src/main/java/com/itwill/dao/OrdersDaoImpl.java
+++ src/main/java/com/itwill/dao/OrdersDaoImpl.java
@@ -1,5 +1,5 @@
 package com.itwill.dao;
 
-public class OrderDaoImpl {
+public class OrdersDaoImpl {
 
 }
