edf02e83343a286be4cafeffdce75790ce74ed9b
daylee619 <daylee619@gmail.com>
2023-10-19 13:37:54 +0900

    Merge branch 'master' of https://github.com/2023-05-JAVA-DEVELOPER-143/2023-05-JAVA-DEVELOPER-final-project-team2.git

--- src/test/java/com/itwill/dao/AdoptDaoImplTest.java
+++ src/test/java/com/itwill/dao/AdoptDaoImplTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.itwill.entity.Adopt;
+import com.itwill.entity.Center;
 import com.itwill.entity.Pet;
 import com.itwill.entity.Userinfo;
 
@@ -25,7 +26,7 @@ class AdoptDaoImplTest {
 	PetDao petDao;
 	@Autowired
 	UserInfoDao userInfoDao;
-	
+
 	
 	
 	
@@ -35,16 +36,28 @@ class AdoptDaoImplTest {
 	//@Disabled
 	void test() {
 		
-		//Userinfo userinfo = userInfoDao.findById("와");
-		//Pet pet = petDao.petFindById(1L);
+		Userinfo userinfo1 = userInfoDao.findById("박태환");
+		Pet pet = Pet.builder()
+				.petNo(null)
+				.petLocal("서울")
+				.petType("포메")
+				.petgender("여")
+				.petCharacter("귀여움")
+				.petFindPlace("서울")
+				.petRegisterDate(LocalDate.now())
+				.center(null)
+				.build();
+		petDao.petInsert(pet);
 		
-		Adopt adopt = Adopt.builder()
+		
+		
+		Adopt adopt = Adopt.builder() 
 							.adoptNo(1L)
 							.adoptDate(LocalDate.now())
 							.adoptTime(11L)
-							.pet(null)
+							.pet(pet)
 							.status("입양완료")
-							.userinfo(null)
+							.userinfo(userinfo1)
 							.build();
 		adoptDao.insertAdopt(adopt);
 		
