efbb9b7e67bcfd8cbe9b9c9217761f03eb56dffc
heoseungbum <heoseungbum@naver.com>
2023-10-19 15:37:05 +0900

    Merge branch 'master' of https://github.com/2023-05-JAVA-DEVELOPER-143/2023-05-JAVA-DEVELOPER-final-project-team2.git

--- src/test/java/com/itwill/dao/CartDaoImplTest.java
+++ src/test/java/com/itwill/dao/CartDaoImplTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.itwill.TeamprojectAnimalcareApplicationTest;
 import com.itwill.entity.Cart;
@@ -11,14 +14,27 @@ import com.itwill.entity.Product;
 import com.itwill.entity.Userinfo;
 
 class CartDaoImplTest extends TeamprojectAnimalcareApplicationTest {
-
+	@Autowired
+	CartDao cartDao;
+	@Autowired
+	UserInfoDao userInfoDao;
+	@Autowired
+	ProductDao productDao;
+	
 	
 	@Test
-	@Disabled
+	@Transactional
+	@Rollback(false)
+	//@Disabled
 	void insertCartTest() {
+		
 		Cart cart = Cart.builder()
-				
+				.cartQty(1)
+				.userinfo(userInfoDao.findById("전아현"))
+				.product(productDao.findByProductNo(1L))
 				.build();
+		
+		cartDao.insertCart(cart);
 	}
 	
 }
