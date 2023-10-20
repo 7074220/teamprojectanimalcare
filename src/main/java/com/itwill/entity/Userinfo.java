f56f238706575c0311210d8b1d0e54c71c37c16d
daylee619 <daylee619@gmail.com>
2023-10-20 11:11:02 +0900

    Merge branch 'master' of https://github.com/2023-05-JAVA-DEVELOPER-143/2023-05-JAVA-DEVELOPER-final-project-team2.git

--- src/main/java/com/itwill/entity/Userinfo.java
+++ src/main/java/com/itwill/entity/Userinfo.java
@@ -72,7 +72,7 @@ public class Userinfo {
 	@Builder.Default
 	List<Volunteer> volunteers = new ArrayList<Volunteer>(); 
 	
-	@OneToMany(mappedBy = "userinfo",cascade = CascadeType.PERSIST,fetch = FetchType.EAGER)
+	@OneToMany(mappedBy = "userinfo",cascade = CascadeType.PERSIST,fetch = FetchType.LAZY)
 	@Builder.Default
 	List<Adopt> adopts = new ArrayList<Adopt>();
 	
