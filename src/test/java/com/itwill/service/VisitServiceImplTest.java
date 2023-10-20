1a3ab05117d5f60d5da3faa167811a833245f1a4
ITWILL <choiyongyong0813@gmail.com>
2023-10-20 12:18:03 +0900

    Merge branch 'master' of https://github.com/2023-05-JAVA-DEVELOPER-143/2023-05-JAVA-DEVELOPER-final-project-team2.git

--- src/test/java/com/itwill/service/VisitServiceImplTest.java
+++ src/test/java/com/itwill/service/VisitServiceImplTest.java
@@ -2,13 +2,55 @@ package com.itwill.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
+import com.itwill.entity.Center;
+import com.itwill.entity.Userinfo;
+import com.itwill.entity.Visit;
+
+import jakarta.transaction.Transactional;
+@SpringBootTest
 class VisitServiceImplTest {
-
+	@Autowired
+	VisitService visitService;
+	
+	@Autowired
+	CenterService centerService;
+	
+	@Autowired
+	UserInfoService userInfoService;
+	
+	@Transactional
+	@Rollback(value = false)
+	@Disabled
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void insertVisit() throws Exception {
+		Visit insertVisit = Visit.builder()
+				.visitNo(null)
+				.userinfo(userInfoService.findUser("김창섭"))
+				.visitTime(3L)
+				.visitDate(LocalDate.now())
+				.center(centerService.findByCenterNo(4L))
+				.visitstatus("접수중")
+				.build();
+		visitService.createVisit(insertVisit);
+	}
+	
+	@Test
+	@Transactional
+	//@Disabled
+	void selectVisit() {
+		Visit selectVisit = visitService.findByVisitNo(1L);
+
+	    
+		    System.out.println(selectVisit);
+		 
 	}
 
 }
