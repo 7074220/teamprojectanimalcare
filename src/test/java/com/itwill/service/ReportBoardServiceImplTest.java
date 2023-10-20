a83c55268f9fdca8bd3956910b6e7964d05ba1c5
heoseungbum <heoseungbum@naver.com>
2023-10-20 10:10:35 +0900

    12312321

--- src/test/java/com/itwill/service/ReportBoardServiceImplTest.java
+++ src/test/java/com/itwill/service/ReportBoardServiceImplTest.java
@@ -3,12 +3,43 @@ package com.itwill.service;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
-class ReportBoardServiceImplTest {
+import com.itwill.TeamprojectAnimalcareApplicationTest;
+import com.itwill.dao.ReportBoardDao;
+import com.itwill.dao.UserInfoDao;
+import com.itwill.entity.ReportBoard;
+import com.itwill.entity.Userinfo;
 
+class ReportBoardServiceImplTest extends TeamprojectAnimalcareApplicationTest{
+	
+	@Autowired
+	UserInfoDao userInfoDao;
+	
+	@Autowired
+	ReportBoardDao reportBoardDao;
+	
 	@Test
+	@Transactional
+	@Rollback(false)
 	void test() {
-		fail("Not yet implemented");
+		Userinfo userinfo = userInfoDao.findById("전아현");
+		ReportBoard reportBoard = ReportBoard.builder()
+									.boardContent("내용")
+									.boardDate(null)
+									.boardDepth(0)
+									.boardGroupNo(1)
+									.boardName(userinfo.getUserId())
+									.boardPhone(userinfo.getUserPhoneNumber())
+									.boardReadCount(0)
+									.boardResisterDate(null)
+									.boardStep(0)
+									.boardTitle("제목")
+									.userinfo(userinfo)
+									.build();
+		reportBoardDao.Create(reportBoard);
 	}
 
 }
