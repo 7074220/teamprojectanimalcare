feb658fc7f742b176897222b9b7bbf599bef0a6e
ITWILL <choiyongyong0813@gmail.com>
2023-10-20 20:11:41 +0900

    이중정렬 빼고 끝

--- src/main/java/com/itwill/dao/ReviewBoardDao.java
+++ src/main/java/com/itwill/dao/ReviewBoardDao.java
@@ -16,17 +16,19 @@ public interface ReviewBoardDao {
 
 	public List<ReviewBoard> findAll();
 
-	List<ReviewBoard> getReviewBoardByProductNo(Long productNo);
-	// productNo로 reviewboard 리스트 검색
+	List<ReviewBoard> getReviewBoardByProductNo(Long productNo); // productNo로 reviewboard 리스트 검색
 
-	public List<ReviewBoard> findByStarAll(Long star); // 선택한 평점과 관련된 리뷰나오기
+	public List<ReviewBoard> findByStarAll(Long star); //선택한 별점으로 찾기
 
-	 public List<ReviewBoard> findAllByUserIdUserinfo(String userId); // 선택된
-	// userId 리뷰 리스트만 나오기
+	 public List<ReviewBoard> findAllByUserIdUserinfo(String userId); // 선택된 userId 리뷰 리스트만 나오기
 
 	public List<ReviewBoard> findAllByOrderByBoardStarDesc(); // 높은 평점순 정렬
 
 	public List<ReviewBoard> findAllByOrderByBoardStarAsc(); // 낮은 평점순 정렬
+
+	public List<ReviewBoard> findAllByOrderByBoardNoDesc(); // 최신순 정렬(board no정렬)
 	
+	public List<ReviewBoard> findAllByOrderByBoardNoAsc(); // 오래된 순 정렬(board no정렬)
 	
-}
+	//public List<ReviewBoard> findByBoardStarOrderByBoardStarDescBoardNoDesc(Long star);//별점 높은순,최신순
+}		
--- src/main/java/com/itwill/dao/ReviewBoardDaoImpl.java
+++ src/main/java/com/itwill/dao/ReviewBoardDaoImpl.java
@@ -9,7 +9,7 @@ import com.itwill.entity.ReviewBoard;
 import com.itwill.repository.ReviewBoardRepository;
 
 @Repository
-public class ReviewBoardDaoImpl implements ReviewBoardDao{
+public class ReviewBoardDaoImpl implements ReviewBoardDao {
 	@Autowired
 	ReviewBoardRepository reviewBoardRepository;
 
@@ -20,7 +20,7 @@ public class ReviewBoardDaoImpl implements ReviewBoardDao{
 
 	@Override
 	public ReviewBoard update(ReviewBoard reviewBoard) {
-		if(reviewBoardRepository.findById(reviewBoard.getBoardNo()).isPresent()) {
+		if (reviewBoardRepository.findById(reviewBoard.getBoardNo()).isPresent()) {
 			reviewBoardRepository.save(reviewBoard);
 		}
 		return reviewBoard;
@@ -28,24 +28,21 @@ public class ReviewBoardDaoImpl implements ReviewBoardDao{
 
 	@Override
 	public void deleteById(Long boardNo) {
-		if(reviewBoardRepository.findById(boardNo).isPresent()) {
+		if (reviewBoardRepository.findById(boardNo).isPresent()) {
 			reviewBoardRepository.deleteById(boardNo);
 		}
-		
-	}
 
+	}
 
 	@Override
 	public List<ReviewBoard> findAll() {
 		return reviewBoardRepository.findAll();
 	}
-	
+
 	@Override
 	public List<ReviewBoard> findByStarAll(Long star) {
 		return reviewBoardRepository.findAllByBoardStar(star);
 	}
-
-
 
 	@Override
 	public List<ReviewBoard> getReviewBoardByProductNo(Long productNo) {
@@ -54,22 +51,42 @@ public class ReviewBoardDaoImpl implements ReviewBoardDao{
 	}
 
 	// userId로 리뷰 리스트 나오기
-	
+
 	@Override
 	public List<ReviewBoard> findAllByUserIdUserinfo(String userId) {
 		return reviewBoardRepository.findAllByUserinfoUserId(userId);
 	}
-	
+
 	// 높은평점순 정렬
 	@Override
 	public List<ReviewBoard> findAllByOrderByBoardStarDesc() {
 		return reviewBoardRepository.findAllByOrderByBoardStarDesc();
 	}
+
 	// 낮은평점순 정렬
 	@Override
 	public List<ReviewBoard> findAllByOrderByBoardStarAsc() {
 		return reviewBoardRepository.findAllByOrderByBoardStarAsc();
 
 	}
+
+	// 최신순 정렬(board no정렬)
+	@Override
+	public List<ReviewBoard> findAllByOrderByBoardNoDesc() {
+		return reviewBoardRepository.findAllByOrderByBoardNoDesc();
+	}
+
+	// 오래된순 정렬(board no정렬)
+	@Override
+	public List<ReviewBoard> findAllByOrderByBoardNoAsc() {
+		return reviewBoardRepository.findAllByOrderByBoardNoAsc();
+	}
 	
+	/*
+	  //별점 높은순,최신순
+	  
+	  @Override public List<ReviewBoard>
+	  findByBoardStarOrderByBoardStarDescBoardNoDesc(Long star) { return
+	  reviewBoardRepository.findByBoardStarOrderByBoardStarDescBoardNoDesc(star); }
+	 */
 }
