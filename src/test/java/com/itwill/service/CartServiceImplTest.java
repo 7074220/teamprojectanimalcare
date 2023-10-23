dc2cd115705f615f6484f94360f7073542dab7c9
ahiivii <axoxoh48@naver.com>
2023-10-23 10:15:52 +0900

    ..

--- src/test/java/com/itwill/service/CartServiceImplTest.java
+++ src/test/java/com/itwill/service/CartServiceImplTest.java
@@ -36,9 +36,82 @@ class CartServiceImplTest extends TeamprojectAnimalcareApplicationTest{
 				.cartNo(null)
 				.cartQty(1)
 				.userinfo(userinfo)
+				.cartImage(product.getProductImage())
 				.build();
 		Cart babo = cartService.insertCart(cart);
 		System.out.println(babo);
 	}
 
+	@Test
+	@Disabled
+	@Transactional
+	@Rollback(false)
+	void updateQtyCartTest() {
+		//Cart update = cartService.update_qty();
+	}
+	
+	@Test
+	@Disabled
+	@Transactional
+	@Rollback(false)
+	void findByCartNoTest() {
+		
+	}
+	
+	@Test
+	@Disabled
+	@Transactional
+	@Rollback(false)
+	void deleteByUserIdTest() {
+		
+	}
+	
+	@Test
+	@Disabled
+	@Transactional
+	@Rollback(false)
+	void deleteByIdTest() {
+		
+	}
+	
+	@Test
+	@Disabled
+	@Transactional
+	@Rollback(false)
+	void findAllTest() {
+		
+	}
+	
+	@Test
+	@Disabled
+	@Transactional
+	@Rollback(false)
+	void cartTotalPriceTest() {
+		
+	}
+	
+	@Test
+	@Disabled
+	@Transactional
+	@Rollback(false)
+	void findAllCartByUserIdTest() {
+		
+	}
+	
+	@Test
+	@Disabled
+	@Transactional
+	@Rollback(false)
+	void countProductByUserIdTest() {
+		
+	}
+	
+	@Test
+	@Disabled
+	@Transactional
+	@Rollback(false)
+	// 카트에 중복제품이 있으면 (중복체크) --> 업데이트 돼서 담기도록 
+	void updateOverlapCart() {
+		
+	}
 }
