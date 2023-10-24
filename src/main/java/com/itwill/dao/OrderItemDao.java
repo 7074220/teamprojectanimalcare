9ec5d3a4af96bf1fe0fc21221d886edb6571b0cd
7074220 <7074220@naver.com>
2023-10-24 11:10:35 +0900

    Merge branch 'master' of https://github.com/2023-05-JAVA-DEVELOPER-143/2023-05-JAVA-DEVELOPER-final-project-team2.git..

--- src/main/java/com/itwill/dao/OrderItemDao.java
+++ src/main/java/com/itwill/dao/OrderItemDao.java
@@ -1,5 +1,38 @@
 package com.itwill.dao;
 
-public class OrderItemDao {
+import java.util.List;
+
+import com.itwill.entity.OrderItem;
+
+public interface OrderItemDao {
+	
+	public OrderItem insertOrderItem(OrderItem orderItem);
+	
+	public OrderItem updateOrderItem(OrderItem orderItem);
+	
+	public OrderItem deleteOrderItem(OrderItem orderItem);
+	
+	public List<OrderItem> findAll();
+	
+
+
 
 }
+/*
+ * int insertOrder(Order order,Payment payment) throws Exception; int
+ * updateOrder(Order order) throws Exception; int deleteOrder(int orderNo)
+ * throws Exception; int insertPayment(Payment payment) throws Exception;
+ * 
+ * int insertOrderItem(OrderItem orderItem) throws Exception; int
+ * updateOrderItem(OrderItem orderItem) throws Exception; int
+ * deleteOrderItem(int orderNo) throws Exception;
+ * 
+ * List<Order> findAllOrders() throws Exception; Order findOrderByNo(int
+ * orderNo) throws Exception; List<Order> findOrdersByDate(Date startDate, Date
+ * endDate) throws Exception; List<Order> findOrdersById(String userId) throws
+ * Exception;
+ * 
+ * List<OrderItem> findAllOrderItems() throws Exception; OrderItem
+ * findOrderItemByNo(int oiNo) throws Exception; List<OrderItem>
+ * findOrderItemsByOrder(int orderNo) throws Exception;
+ */
