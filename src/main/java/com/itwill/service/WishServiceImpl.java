2ef68ec733ef37ba608eced3252215bc0d0abfc4
ahiivii <axoxoh48@naver.com>
2023-10-25 10:08:39 +0900

    wish service 구현 중~

--- src/main/java/com/itwill/service/WishServiceImpl.java
+++ src/main/java/com/itwill/service/WishServiceImpl.java
@@ -1,5 +1,41 @@
 package com.itwill.service;
 
-public class WishServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.itwill.dao.CartDao;
+import com.itwill.dao.UserInfoDao;
+import com.itwill.dao.WishDao;
+import com.itwill.entity.Wish;
+import com.itwill.repository.WishRepository;
+
+public class WishServiceImpl implements WishService {
+
+	@Autowired
+	private WishRepository wishrepoRepository;
+	@Autowired
+	private CartDao cartDao;
+	@Autowired
+	private UserInfoDao userinfoDao;
+	
+	@Override
+	public Wish insertWish(Wish wish) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteWish(Long no) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Wish> findAllWishByUserNo(Long userNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 }
