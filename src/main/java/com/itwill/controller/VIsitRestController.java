504c1e27f596c21e0bd68942702b5ad24e9a0a39
ITWILL <choiyongyong0813@gmail.com>
2023-10-27 15:18:20 +0900

    Merge branch 'master' of https://github.com/2023-05-JAVA-DEVELOPER-143/2023-05-JAVA-DEVELOPER-final-project-team2.git

--- src/main/java/com/itwill/controller/VIsitRestController.java
+++ src/main/java/com/itwill/controller/VIsitRestController.java
@@ -1,6 +1,7 @@
 package com.itwill.controller;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +24,8 @@ import com.itwill.dto.VisitDto;
 import com.itwill.entity.Visit;
 import com.itwill.service.VisitService;
 
-
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.persistence.criteria.Path;
 import jakarta.servlet.http.HttpSession;
 
 @RestController
@@ -32,7 +35,7 @@ public class VIsitRestController {
 	@Autowired
 	private VisitService visitService ;
 	
-	
+	@Operation(summary = "견학신청")
 	@PostMapping
 	public ResponseEntity<VisitDto> createVisit(@RequestBody VisitDto dto, HttpSession session) {
 		//견학 생성
@@ -42,31 +45,25 @@ public class VIsitRestController {
 		httpHeaders.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
 		return new ResponseEntity<>(dto, httpHeaders, HttpStatus.CREATED);
 	}
+	@Operation(summary = "견학리스트조회")
+	@GetMapping("/visits")
+	public ResponseEntity<List<VisitDto>> visitList() {
+		//모든 방문 리스트 조회
+		List<Visit> visits = visitService.selectAllVisits();
+		List<VisitDto> visitDtoList = new ArrayList<VisitDto>();
+		for(Visit visit : visits) {
+			visitDtoList.add(VisitDto.toDto(visit));
+		}
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+		return new ResponseEntity<List<VisitDto>>(visitDtoList, httpHeaders, HttpStatus.OK);
+	}
+
+	@Operation(summary = "견학리스트삭제")
+	@DeleteMapping("/{visitNo}")
+	public void VisitDelete(@PathVariable(name = "visitNo") Long visitNo) {
+		visitService.deleteVisit(visitNo);
+	}
 	
-//	@GetMapping("/all")
-//	public ResponseEntity<List<Visit>> getAllVisits() {
-//		//모든 방문 리스트 조회
-//		List<Visit> visits = visitService.selectAllVisits();
-//		return ResponseEntity.ok(visits);
-//	}
-//	@PutMapping("/{visitNo}")
-//	public ResponseEntity<Visit> updateVisit (@PathVariable Long visitNo, @RequestBody Visit updatedVisit) {
-//		// 센터수정
-//		Visit SearchVisit = visitService.findByVisitNo(visitNo);
-//		SearchVisit.setVisitDate(updatedVisit.getVisitDate());
-//		SearchVisit.setVisitStatus(updatedVisit.getVisitStatus());
-//		SearchVisit.setVisitTime(updatedVisit.getVisitTime());
-//		
-//		Visit saveVisit = visitService.updateVisit(SearchVisit);
-//		
-//		return ResponseEntity.ok(saveVisit);
-//	}
-//	
-//	
-//	public ResponseEntity<Visit> daleteVisit (@PathVariable Long visitNo) {
-//		visitService.deleteVisit(visitNo);
-//		//견학 삭제
-//		
-//		return ResponseEntity.noContent().build();
-//	}
 }
