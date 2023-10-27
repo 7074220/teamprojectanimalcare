0763acaaa788d753876b51fc5038b0d6f2205f6b
seo0ojin <seo0ojinnn@gmail.com>
2023-10-27 19:14:55 +0900

    ..

--- src/main/java/com/itwill/controller/CartRestController.java
+++ src/main/java/com/itwill/controller/CartRestController.java
@@ -10,10 +10,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itwill.dto.CartDto;
+import com.itwill.entity.Cart;
 import com.itwill.service.CartService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -38,24 +41,43 @@ public class CartRestController {
 	}
 
 	@Operation(summary = "카트 번호로 한 개 삭제")
-	@DeleteMapping("/{productNo}")
-	public ResponseEntity<CartDto> deleteByCartNo(@PathVariable(name = "productNo") Long productNo) throws Exception {
+	@DeleteMapping("/delete/{cartNo}")
+	public void deleteByCartNo(@PathVariable(name = "cartNo") Long cartNo) throws Exception {
 
-		HttpHeaders httpHeaders = new HttpHeaders();
-
-		cartService.deleteById(productNo);
-
-		return new ResponseEntity<CartDto>(httpHeaders, HttpStatus.OK);
+		cartService.deleteById(cartNo);
 	}
 
 	@Operation(summary = "유저 아이디로 카트 전체 삭제")
-	@DeleteMapping("/{userNo}")
+	@DeleteMapping("/delete/user/{userNo}")
 	public ResponseEntity<CartDto> deleteByUserNo(@PathVariable(name = "userNo") Long userNo) throws Exception {
 
 		HttpHeaders httpHeaders = new HttpHeaders();
 
-		cartService.deleteById(userNo);
+		cartService.deleteByUserId(userNo);
 
 		return new ResponseEntity<CartDto>(httpHeaders, HttpStatus.OK);
+	}
+	
+	@Operation(summary = "상품 수량 업데이트")
+	@PutMapping("/{cartNo}")
+	public ResponseEntity<CartDto> updateCartQty(@PathVariable(name = "cartNo") Long no, @RequestBody CartDto dto) throws Exception {
+		Cart findCart = cartService.findByCartNo(no);
+	
+		findCart.setCartQty(dto.getCartQty());
+		cartService.update_qty(findCart);
+		
+		CartDto updatedDto = CartDto.toDto(findCart);
+		
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+		
+		return new ResponseEntity<CartDto>(updatedDto, httpHeaders, HttpStatus.OK);
+	}
+	
+	@Operation(summary = "중복 상품 업데이트")
+	@PutMapping("/update/{cartNo}")
+	public ResponseEntity<CartDto> updateOverlapCart() {
+	
+		return null;
 	}
 }
