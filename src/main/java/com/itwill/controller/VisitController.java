bf3815b94b5388a5f58aea7de23cf7bbf84f2dcf
ITWILL <choiyongyong0813@gmail.com>
2023-10-30 18:45:12 +0900

    센터,견학 레이아웃 삽입완료

---
+++ src/main/java/com/itwill/controller/VisitController.java
@@ -0,0 +1,38 @@
+package com.itwill.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.itwill.entity.Center;
+import com.itwill.entity.Visit;
+import com.itwill.service.CenterService;
+import com.itwill.service.VisitService;
+
+@Controller
+@RequestMapping()
+public class VisitController {
+	
+	@Autowired
+	VisitService visitService;
+	
+	//견학신청
+	@GetMapping("/visitApply")
+	public String apply(Model model) throws Exception {
+		return "";
+	}
+	
+	//visitlist 전체출력(미완성)
+    @GetMapping("/visitList")
+    public String centerList(Model model) {
+        List<Visit> visitList = visitService.selectAllVisits();
+        model.addAttribute("visiits", visitList);
+        return "";
+	}
+    
+     
+}
