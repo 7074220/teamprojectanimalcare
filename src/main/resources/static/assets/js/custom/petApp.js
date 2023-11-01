efa3cc9e350352509af857c11c53677a424c4744
윤호진 <ghwls1997@naver.com>
2023-11-01 16:11:41 +0900

    pet list 작성진행중

---
+++ src/main/resources/static/assets/js/custom/petApp.js
@@ -0,0 +1,81 @@
+import {petList} from './template-pet-list';
+import {ajaxRequest} from './request.js';
+
+//작성진행중
+let hash = window.location.hash
+let path = hash.substring(1);
+let html = '';
+
+/*
+초기실행메쏘드
+*/
+function init() {
+	registEvent();
+	navigate();
+}
+/*
+이벤트등록
+*/
+function registEvent() {
+	/*
+	$(window).on('load', function(e) {
+		alert('load  event:' + e);
+	});
+	*/
+	$(window).on('hashchange', function(e) {
+		alert('hashchange event:' + e);
+		hash = window.location.hash
+		path = hash.substring(1);
+		navigate();
+	});
+	
+	$(document).on('click', function(e) {
+		if ($(e.target).attr('data-navigate')) {
+			alert('click event --> hash변경:' + $(e.target).attr('data-navigate'));
+			// 해쉬 변경
+			window.location.hash = e.target.getAttribute('data-navigate');
+		}
+		
+	$(document).ready(function(){
+    $('#short').change(function(){
+        var selectedValue = $(this).val();  // 선택된 값을 가져옵니다.
+
+        // 선택된 값을 기반으로 새로운 해시를 설정합니다.
+        window.location.hash = selectedValue;
+        
+        path = hash.substring(1);
+    });
+});
+		
+	});
+}
+
+/*
+	이벤트발생시 처리메쏘드
+*/
+function navigate() {
+	if (path == '/user_write_form') {
+		window.location.href='register'
+		html = user_write_form();
+		$('#content').html(html);
+	}
+	if (path == '/user_write_action') {
+		/**************** /user_write_action******************/
+		let sendJsonObject = {
+				userId: document.f.userId.value,
+				userPassword: document.f.password.value,
+				userName: document.f.name.value,
+				userGender: document.f.gender.value,
+				userPhoneNumber: document.f.phone.value,
+				userAddress: document.f.address.value
+		}
+		const responseJsonObject = ajaxRequest('POST','user',sendJsonObject);
+		html = user_write_form(responseJsonObject);
+		$('#content').html(html);
+	}
+}
+
+
+
+init();
+
