99065782d849d20b35a03fdae62bea5efd0d61a5
daylee619 <daylee619@gmail.com>
2023-11-02 17:39:20 +0900

    Merge branch 'master' of https://github.com/2023-05-JAVA-DEVELOPER-143/2023-05-JAVA-DEVELOPER-final-project-team2.git

--- src/main/java/com/itwill/controller/AdoptController.java
+++ src/main/java/com/itwill/controller/AdoptController.java
@@ -1,10 +1,12 @@
 package com.itwill.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,36 +34,48 @@ public class AdoptController {
 	// 입양신청
 	@GetMapping(value = "/adopt", params = "petNo")
 	public String apply(Model model, @RequestParam Long petNo) {
-		Pet pet=petService.petFindById(petNo);
+		Pet pet = petService.petFindById(petNo);
 		model.addAttribute("pet", pet);
+		return "adopt";
+	}
+
+	@PostMapping("/create-adopt")
+	public String createAdopt(@RequestParam("adoptDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date adoptDate,
+			@RequestParam("adoptTime") int selectedHour, @RequestParam Long petNo, Model model) {
+		Adopt adopt = new Adopt();
+		adopt.setAdoptDate(adoptDate);
+		adopt.setAdoptTime(selectedHour);
+		adopt.setAdoptStatus("입양신청접수");
+
+		Pet pet = petService.petFindById(petNo);
+		adopt.setPet(pet);
+		adoptService.insertAdopt(adopt);
+
 		return "adopt";
 	}
 
 	// 입양 리스트 조회(관리자)
 	@GetMapping("/adoptList")
 	public String adoptList(Model model) {
-		//List<AdoptDto> adoptDtoList = new ArrayList<>();
+		// List<AdoptDto> adoptDtoList = new ArrayList<>();
 		List<Adopt> adoptList = adoptService.findAdoptList();
 		/*
-		for (Adopt adopt : adoptList) {
-			adoptDtoList.add(AdoptDto.fromEntity(adopt));
-		}
-		model.addAttribute("adoptDtoList", adoptDtoList);
-		*/
+		 * for (Adopt adopt : adoptList) { adoptDtoList.add(AdoptDto.fromEntity(adopt));
+		 * } model.addAttribute("adoptDtoList", adoptDtoList);
+		 */
 		model.addAttribute("adoptList", adoptList);
 		return "my-account";
 	}
 
-	//userNo에 따른 입양리스트 조회
+	// userNo에 따른 입양리스트 조회
 	@GetMapping("/adopt/user/{userNo}")
 	public String findByUserNoAdoptList(Model model, @PathVariable(name = "userNo") Long userNo) {
-		List<Adopt> adoptList=adoptService.findAdoptsByUserNo(userNo);
-		//List<AdoptDto> adoptDtoUserNoList = new ArrayList<>();
+		List<Adopt> adoptList = adoptService.findAdoptsByUserNo(userNo);
+		// List<AdoptDto> adoptDtoUserNoList = new ArrayList<>();
 		/*
-		for (Adopt adopt : adoptList) {
-			adoptDtoUserNoList.add(AdoptDto.fromEntity(adopt));
-		}
-		*/
+		 * for (Adopt adopt : adoptList) {
+		 * adoptDtoUserNoList.add(AdoptDto.fromEntity(adopt)); }
+		 */
 		model.addAttribute("adoptList", adoptList);
 		return "my-account";
 	}
@@ -69,12 +83,9 @@ public class AdoptController {
 	// adoptNo 입양 조회
 	@GetMapping("/adopt/{adoptNo}")
 	public String findByAdoptNoAdopt(Model model, @PathVariable(name = "adoptNo") Long adoptNo) {
-		Adopt adopt=adoptService.findByAdoptNo(adoptNo);
+		Adopt adopt = adoptService.findByAdoptNo(adoptNo);
 		model.addAttribute("adopt", adopt);
 		return "my-account";
 	}
-	
 
-	
-	
 }
