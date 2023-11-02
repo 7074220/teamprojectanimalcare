8d2b9bb01d00bcde0635267446fb2b6638fc8d03
choiyongyong0813 <choiyongyong0813@gmail.com>
2023-11-02 17:18:30 +0900

    견학 신청폼 유저정보만 뺴고 나머지 db에 삽입가능

--- src/main/java/com/itwill/controller/VisitController.java
+++ src/main/java/com/itwill/controller/VisitController.java
@@ -1,18 +1,29 @@
 package com.itwill.controller;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.itwill.dto.VisitDto;
 import com.itwill.entity.Center;
+import com.itwill.entity.Userinfo;
 import com.itwill.entity.Visit;
 import com.itwill.service.CenterService;
+import com.itwill.service.UserInfoService;
 import com.itwill.service.VisitService;
+
+import groovy.transform.AutoImplement;
 
 @Controller
 public class VisitController {
@@ -21,20 +32,40 @@ public class VisitController {
 	private VisitService visitService;
 	@Autowired
 	private CenterService centerService;
+	@Autowired
+	private UserInfoService userInfoService;
 
 	// 견학신청
 	@GetMapping(value = "/visit", params = "centerNo")
-	public String apply(Model model,@RequestParam Long centerNo) throws Exception {
+	public String apply(Model model, @RequestParam Long centerNo) throws Exception {
 		Center center = centerService.findByCenterNo(centerNo);
+		// Userinfo userinfo = userInfoService.findUserByNo(userNo);
+
+		// model.addAttribute("userinfo", userinfo );
 		model.addAttribute("center", center);
 		return "visit";
 	}
-	
+
+	@PostMapping("/create-visit")
+	public String createVisit(@RequestParam("visitDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date visitDate,
+			@RequestParam("visitTime") int selectedHour, @RequestParam Long centerNo, Model model) {
+
+		Visit visit = new Visit();
+		visit.setVisitDate(visitDate);
+		visit.setVisitStatus("접수중");
+		visit.setVisitTime(selectedHour);
+
+		Center center = centerService.findByCenterNo(centerNo);
+	    visit.setCenter(center);
+		visitService.createVisit(visit);
+
+		return "center-list";
+	}
 
 	// 관리자 견학리스트 전체출력
 	@GetMapping("/visitList")
 	public String centerList(Model model) {
-		List<Visit> visits  = visitService.selectAllVisits();
+		List<Visit> visits = visitService.selectAllVisits();
 		model.addAttribute("visits", visits);
 		return "my-account";
 	}
@@ -46,5 +77,5 @@ public class VisitController {
 		model.addAttribute("memberVisits", memberVisitList);
 		return "my-account";
 	}
-	
+
 }
