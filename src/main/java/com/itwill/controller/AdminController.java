ee1bd14b3783d4aef63418db1f5e9d8df4c5954b
pains <pains@172.30.1.20>
2023-11-12 19:15:00 +0900

    수정

--- src/main/java/com/itwill/controller/AdminController.java
+++ src/main/java/com/itwill/controller/AdminController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -233,6 +234,7 @@ public class AdminController {
 		}
 		*/
 		
+		@Transactional
 		@GetMapping("/updateVolunteer/{volunteerNo}")
 		public String updateVolunteer(@PathVariable Long volunteerNo, Model model, HttpSession session) throws Exception {
 			try {
