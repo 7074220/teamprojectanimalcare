7f3ff9e1dd4a5d1b1e64ffda5a5885a1ef1439df
장희주 <7074220@naver.com>
2024-04-13 17:58:14 +0900

    변경

--- src/main/java/com/itwill/controller/CartController.java
+++ src/main/java/com/itwill/controller/CartController.java
@@ -1,22 +1,6 @@
 package com.itwill.controller;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.itwill.dto.CartDto;
-import com.itwill.dto.OrdersDto;
 import com.itwill.dto.ProductListDto;
-import com.itwill.dto.ProductNameDto;
 import com.itwill.entity.Cart;
 import com.itwill.entity.MyPet;
 import com.itwill.entity.Product;
@@ -25,49 +9,56 @@ import com.itwill.service.CartService;
 import com.itwill.service.MyPetService;
 import com.itwill.service.ProductService;
 import com.itwill.service.UserInfoService;
-
 import jakarta.servlet.http.HttpSession;
-import oracle.net.aso.b;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CartController {
 
-	@Autowired
-	private CartService cartService;
-	@Autowired
-	private ProductService productService;
-	@Autowired
-	private UserInfoService userinfoService;
-	@Autowired
-	private MyPetService myPetService;
-	
-	@GetMapping("/cartList")
-	// 카트 리스트 보기 (유저)
-	public String cartList(Model model, HttpSession session) throws Exception{
-			if (session.getAttribute("userNo") == null) {
-				throw new Exception("로그인 하세요.");
-			}
-		
-		Long userNo=(Long)session.getAttribute("userNo");
-		
-		//List<CartDto> cartListDto = new ArrayList<>();
-		List<Cart> cartList = cartService.findAllCartByUserId(userNo);
-		Integer totalPrice = 0;
+    @Autowired
+    private CartService cartService;
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private UserInfoService userinfoService;
+    @Autowired
+    private MyPetService myPetService;
+
+    @GetMapping("/cartList")
+    // 카트 리스트 보기 (유저)
+    public String cartList(Model model, HttpSession session) throws Exception {
+        if (session.getAttribute("userNo") == null) {
+            throw new Exception("로그인 하세요.");
+        }
+
+        Long userNo = (Long) session.getAttribute("userNo");
+
+        //List<CartDto> cartListDto = new ArrayList<>();
+        List<Cart> cartList = cartService.findAllCartByUserId(userNo);
+        Integer totalPrice = 0;
 		
 		/*
 		for (Cart cart : cartList) {
 			cartListDto.add(CartDto.toDto(cart));
 		}
 		*/
-		for (Cart cart : cartList) {
-			totalPrice = totalPrice + cart.getProduct().getProductPrice() * cart.getCartQty();
-		}
-		
-		model.addAttribute("cartList", cartList);
-		model.addAttribute("totalPrice", totalPrice);
-		
-		return "cart";
-	}
+        for (Cart cart : cartList) {
+            totalPrice = totalPrice + cart.getProduct().getProductPrice() * cart.getCartQty();
+        }
+
+        model.addAttribute("cartList", cartList);
+        model.addAttribute("totalPrice", totalPrice);
+
+        return "cart";
+    }
 	
 	/*
 	@GetMapping(value = "/cart")
@@ -130,88 +121,79 @@ public class CartController {
 		return "product-details";
 	}
 	*/
-	
-	
-	@GetMapping(value = "/insertCartMain")
-	// 상품리스트에서 카트에 담기
-	public String insertCartMain(Model model, HttpSession session, @RequestParam Long productNo, @RequestParam Integer productQty) throws Exception{
-		if (session.getAttribute("userNo") == null) {
-			throw new Exception("로그인 하세요.");
-		}
-		
-		// session에서 userNo가져오기
-		Long userNo=(Long)session.getAttribute("userNo");
-		// userNo로 user 찾기
-		Userinfo user = userinfoService.findUserByNo(userNo);
-		// productNo로 product 정보 가져오기
-		Product product = productService.findByProductNo(productNo);
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+productNo);
-		
-		Cart selectCart = Cart.builder().build();
-		selectCart.setUserinfo(user);
-		selectCart.setProduct(product);
-		selectCart.setCartQty(productQty);
-		//selectCart.setCartQty(product.getProductQty());
-		
-		cartService.updateOverlapCart(selectCart);
-		
-		//List<Cart> cartList = cartService.findAllCartByUserId(userNo); 
-		
-		
-		model.addAttribute("cart", selectCart);
-		
-		List<ProductListDto> productListDto = new ArrayList<>();
-		List<Product> productList = new ArrayList<>();
-		
-		MyPet myPet = MyPet.builder().build();
-		
-		productList = productService.findAllByOrderByProductNoDesc();
-		
-		if(userNo != null) {
-			myPet = myPetService.findLeaderMyPet(userNo);
-			if (myPet == null) {
-				myPet = MyPet.builder().build();
-			} else if (product.getProductPetCategory().equals("강아지")) {
-				myPet.setMypetKind("강아지");
-				productList = productService.findAllProductByPetCategory("강아지");
-			} else if (product.getProductPetCategory().equals("고양이")) {
-				myPet.setMypetKind("고양이");
-				productList = productService.findAllProductByPetCategory("고양이");
-			}
-		}
-		
-		for (Product products : productList) {
-			productListDto.add(ProductListDto.toDto(products));
-		}
-		
-		int cartCount = cartService.findAllCartByUserId(userNo).size();
-		session.setAttribute("cartCount", cartCount);
-		model.addAttribute("productList", productListDto);
-		model.addAttribute("myPet", myPet);
-		
-		
-		return "shop";
-	}
-	
-	@PostMapping("/delete")
-	public String delete(@RequestParam List<String> cartNo) throws Exception {
-		
-		for (int i = 0; i < cartNo.size(); i++) {
-			Long no = Long.valueOf(cartNo.get(i));
-			cartService.deleteById(no);
-		}
-		
-		return "cart";
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+    @GetMapping(value = "/insertCartMain")
+    // 상품리스트에서 카트에 담기
+    public String insertCartMain(Model model, HttpSession session, @RequestParam Long productNo, @RequestParam Integer productQty) throws Exception {
+        if (session.getAttribute("userNo") == null) {
+            throw new Exception("로그인 하세요.");
+        }
+
+        // session에서 userNo가져오기
+        Long userNo = (Long) session.getAttribute("userNo");
+        // userNo로 user 찾기
+        Userinfo user = userinfoService.findUserByNo(userNo);
+        // productNo로 product 정보 가져오기
+        Product product = productService.findByProductNo(productNo);
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + productNo);
+
+        Cart selectCart = Cart.builder().build();
+        selectCart.setUserinfo(user);
+        selectCart.setProduct(product);
+        selectCart.setCartQty(productQty);
+        //selectCart.setCartQty(product.getProductQty());
+
+        cartService.updateOverlapCart(selectCart);
+
+        //List<Cart> cartList = cartService.findAllCartByUserId(userNo);
+
+
+        model.addAttribute("cart", selectCart);
+
+        List<ProductListDto> productListDto = new ArrayList<>();
+        List<Product> productList = new ArrayList<>();
+
+        MyPet myPet = MyPet.builder().build();
+
+        productList = productService.findAllByOrderByProductNoDesc();
+
+        if (userNo != null) {
+            myPet = myPetService.findLeaderMyPet(userNo);
+            if (myPet == null) {
+                myPet = MyPet.builder().build();
+            } else if (product.getProductPetCategory().equals("강아지")) {
+                myPet.setMypetKind("강아지");
+                productList = productService.findAllProductByPetCategory("강아지");
+            } else if (product.getProductPetCategory().equals("고양이")) {
+                myPet.setMypetKind("고양이");
+                productList = productService.findAllProductByPetCategory("고양이");
+            }
+        }
+
+        for (Product products : productList) {
+            productListDto.add(ProductListDto.toDto(products));
+        }
+
+        int cartCount = cartService.findAllCartByUserId(userNo).size();
+        session.setAttribute("cartCount", cartCount);
+        model.addAttribute("productList", productListDto);
+        model.addAttribute("myPet", myPet);
+
+
+        return "shop";
+    }
+
+    @PostMapping("/delete")
+    public String delete(@RequestParam List<String> cartNo) throws Exception {
+
+        for (int i = 0; i < cartNo.size(); i++) {
+            Long no = Long.valueOf(cartNo.get(i));
+            cartService.deleteById(no);
+        }
+
+        return "cart";
+    }
+
+
 }
--- src/main/java/com/itwill/dto/MyPetListDto.java
+++ src/main/java/com/itwill/dto/MyPetListDto.java
@@ -1,17 +1,9 @@
 package com.itwill.dto;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
-
-import com.itwill.entity.MyPet;
-import com.itwill.entity.Userinfo;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import oracle.net.aso.b;
 
 @Data
 @Builder
@@ -19,13 +11,13 @@ import oracle.net.aso.b;
 @NoArgsConstructor
 public class MyPetListDto {
 
-	private Long mypetNo;
-	private int mypetSequence;
-	private String mypetName;
-	private String mypetBirthday;
-	private String mypetKind;
-	
-	
-	private String mypetLeader;
-	
+    private Long mypetNo;
+    private int mypetSequence;
+    private String mypetName;
+    private String mypetBirthday;
+    private String mypetKind;
+
+
+    private String mypetLeader;
+
 }
